02b9f7c04735d8d60ba5ef7eec695eed1cdde64f
Manish goud <kemanishkodumur@gmail.com>
2025-12-12 18:44:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict" // treats all code as newer version of JavaScript
+
+
+alert(3+3) //we are using nodejs not browser so alert will not work
+
+console.log(3
+    +3) // conde should be readable
+
+
+let name = "John"
+let age = 25
+let isAdmin = true
+
+// datatypes
+// 1. number
+// 2. string
+// 3. boolean
+// 4. null
+// 5. undefined
+// 6. object
+// 7. symbol (ES6)
+// 8. bigint (ES2020)
+
+// typeof operator
+
+// console.log(typeof name) // string
+// console.log(typeof age) // number
+// console.log(typeof isAdmin) // boolean
+
+// let x = null
+// console.log(typeof x) // object (this is a bug in JavaScript)
+
+// let y
+// console.log(typeof y) // undefined
