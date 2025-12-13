733512ee06a953a72af205b83687d3d8a850dd6e
Manish goud <kemanishkodumur@gmail.com>
2025-12-13 14:10:19 +0000

    arrays 02

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,23 @@
+let arr1 = ["thor","spiderman","Ironman"]
+let arr2 = ["superman","flash","batman"]
+
+// arr1.push(arr2) 
+// console.log(arr1) // ["thor","spiderman","Ironman",  ["superman","flash","batman"]]
+
+// let arr3 = arr1.concat(arr2)
+// console.log(arr3)
+
+// let arr4 = [...arr1,...arr2] // spreading the array values and concating the arrays used mostly because we can concat multiple arrays
+// console.log(arr4);
+
+// let arr5 = [1,2,3,[4,5,6],7,[8,9,[2,3]]]
+// let arr6 = arr5.flat(Infinity) // spreads the values 
+
+// console.log(Array.isArray("Maneesh")) // verifies if array
+// console.log(Array.from("Maneesh")) // individual characters are made into an array
+// ***** // console.log(Array.from({name : Maneesh})) // returns [] as it cannot understand to form an array from keys or the values
+
+// let a = 10
+// let b = 20
+// let c = 30
+// console.log(Array.of(a,b,c)) // makes array of the given values
