3abb6e2f0e54e63966ce65fe3499ea9a1ab6144d
Manish goud <kemanishkodumur@gmail.com>
2025-12-13 03:32:50 +0000

    operations and conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,28 +1,56 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score) // string
+// console.log(typeof score) // string
 
-let value = Number(score) // conversion from string to number
-console.log(typeof value)
+// let value = Number(score) // conversion from string to number
+// console.log(typeof value)
 
-// "33" => 33
-// "33abc" => NaN(Not a Number)
-// true => 1
-// false => 0
+// // "33" => 33
+// // "33abc" => NaN(Not a Number)
+// // true => 1
+// // false => 0
 
-let isLoggedIn = 1
-let booleanValue = Boolean(isLoggedIn) // conversion from number to boolean
-console.log(booleanValue) // true
+// let isLoggedIn = 1
+// let booleanValue = Boolean(isLoggedIn) // conversion from number to boolean
+// console.log(booleanValue) // true
 
-// "" => false
-// "abc" => true
-// 0 => false
-// 1 => true
+// // "" => false
+// // "abc" => true
+// // 0 => false
+// // 1 => true
 
-let num = 55
-let str = String(num) // conversion from number to string
-console.log(typeof str) // string
+// let num = 55
+// let str = String(num) // conversion from number to string
+// console.log(typeof str) // string
 
-// 55 => "55"
-// true => "true"
-// false => "false"
+// // 55 => "55"
+// // true => "true"
+// // false => "false"
+
+//****************************** Operations **************************************
+
+// let num =3
+// let negValue = -num
+// console.log(negValue)
+
+// console.log(3 + 2) 
+// console.log(3-2)
+// console.log(3*2)
+// console.log(3/2)
+// console.log(3%2)
+// console.log(3**2)
+
+// let str1 = "Hello"
+// let str2 = "World"
+
+// console.log(str1 + " " + str2) // concatenation
+
+// console.log("Hello" + 5) // Hello5
+// console.log("5" + 2 + 2) // 522
+// console.log(2 + 2 + "5") // 45
+
+// console.log(+true)   // 1
+// console.log(+false)  // 0
+// console.log(+"")     // 0
+// console.log(+"34")   // 34
+
