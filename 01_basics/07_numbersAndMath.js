5f23ed49399ebe16c672e1481e4b10d2621b4d0b
Manish goud <kemanishkodumur@gmail.com>
2025-12-13 06:23:53 +0000

    numbes and math methods

---
+++ 01_basics/07_numbersAndMath.js
@@ -0,0 +1,33 @@
+// const n = 100
+// console.log(n)
+
+// const balance = new Number(400)
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+
+// const m = 33245.9987665
+// console.log(m.toPrecision(3)) // moves decimal to the left hand side
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// let i = Number.MAX_VALUE // defines the largest possible value
+// let j = Number.MIN_VALUE // defines the Smallest possible value
+// console.log(i)
+// console.log(j)
+
+// ************************** MATH *********************************************************
+// console.log(Math.abs(-4)) // converts to +ve number
+// console.log(Math.round(4.6)) // rounds the value
+// console.log(Math.ceil(4.3)) // rounds to upper value (5)
+// console.log(Math.floor(4.7)) // rounds to lower value (4)
+// console.log(Math.random()) // values b/w 0 and 1
+// console.log(Math.floor(Math.random()*10) + 1) // values b/w 1 and 10
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1) + min))
+
