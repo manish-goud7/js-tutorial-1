2b4240a091224c9c2abb1e689574cfca81083585
Manish goud <kemanishkodumur@gmail.com>
2025-12-13 06:57:23 +0000

    dates and time in javascript

---
+++ 01_basics/08_datesAndTime.js
@@ -0,0 +1,29 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate) //  object
+
+// let createdDate = new Date(2025,0,25,5,2) // months start from 0 i.e january (format - year,month,date,hours,minutes,seconds)
+// let createdDate = new Date("2025-01-25") // Another way or representing date here month starts from 1 itself
+// let createdDate = new Date("01-25-2025") 
+// console.log(createdDate.toLocaleString())
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(createdDate.getTime()); // in ms
+// console.log(Math.floor(myTimeStamp/1000)) //in sec 
+
+// let newDate = new Date()
+// console.log(newDate.toLocaleString)
+// console.log(newDate.getMonth()+1)
+// console.log(newDate.getDay())
+
+
+// newDate.toLocaleString('default',{
+//     weekday : "long"
+// })
